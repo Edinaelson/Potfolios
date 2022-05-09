533a5011d7cbd1d032ab55bc2738a52a0045788b
Edinaelson <sedinaelson@gmail.com>
2022-05-08 23:07:38 -0300

    menu

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+import java.util.Calendar;
 import java.util.Scanner;
 
 public class Main {
@@ -11,6 +12,7 @@ public class Main {
 		
 		Scanner input = new Scanner(System.in);
 		String tipoCliente;
+		Calendar  calendar = Calendar.getInstance();
 		
 		System.out.println("|--------------Menu----------------------|");
 		System.out.println("|Digite-1: Regular-||-Digite-2-Fidelidade|");
@@ -20,7 +22,23 @@ public class Main {
 		}else {
 			tipoCliente = "Reward";
 		}
+		System.out.println("|Informe dia:----------------------------|");
+		int dia = input.nextInt();
+		System.out.println("|Informe mes:----------------------------|");
+		int mes = input.nextInt();
+		System.out.println("|Informe ano:----------------------------|");
+		int ano = input.nextInt();
+		calendar.set(ano,mes,dia);
+		//System.out.println("Dia: " + calendar.get(calendar.DAY_OF_WEEK));
+		int diaSemana = calendar.get(calendar.DAY_OF_WEEK);
+		int diaMes = calendar.get(calendar.DAY_OF_MONTH);
+		System.out.println("DIa do mes: " + diaMes);
+		System.out.println("dia da semana: " + diaSemana);
+		int mesano = calendar.get(calendar.MONTH);
+		System.out.println("mes: " + mesano);
+		
+		
 		
 	}
-
+	
 }
